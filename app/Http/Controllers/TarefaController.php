4af7216d923728e10786095f8036d3ad0f463896
Ayrton Gomes <ayrton.gomes@live.com>
2022-04-07 01:48:12 -0300

    Adicionando paginação no index

--- app/Http/Controllers/TarefaController.php
+++ app/Http/Controllers/TarefaController.php
@@ -19,7 +19,7 @@ class TarefaController extends Controller
      */
     public function index(Request $request)
     {
-        $lista_tarefas =  Tarefa::with(['TarefaStatus'])->paginate(2);
+        $lista_tarefas =  Tarefa::with(['TarefaStatus'])->paginate(4);
         return view('site.index', ['lista_tarefas' => $lista_tarefas, 'request' => $request->all()])
         ->with('lista_tarefas', $lista_tarefas);
     }
