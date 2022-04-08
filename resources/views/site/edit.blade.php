ec30b349c2c57b0664aae372a6fe1fc6af8f1b1c
Ayrton Gomes <ayrton.gomes@live.com>
2022-04-08 18:02:37 -0300

    Alteração no layout de editar tarefa

--- resources/views/site/edit.blade.php
+++ resources/views/site/edit.blade.php
@@ -5,31 +5,36 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <title>Editar Tarefa</title>
 </head>
 
 <body>
-    <h1> Editar Tarefa</h1> <a href="{{route('tarefas.index')}}"> <button class="botao-voltar">Voltar </button> </a> <br> <br> <br> <br> <br>
-    <form name="editar-tarefa" method="POST" action="{{route('tarefas.update', ['tarefa' => $tarefa->id ])}}">
+    <p class="fs-1 text-center"> Editar Tarefa <a href="{{route('tarefas.index')}}">
+            <p class="fs-4 text-center"><a href="{{route('tarefas.index')}}"> Voltar </a> </p>
+            <hr>
+            </hr>
+            <div class="text-center">
+                <form name="editar-tarefa" method="POST" action="{{route('tarefas.update', ['tarefa' => $tarefa->id ])}}">
 
-        @csrf
-        @method('PUT')
-        <label> <h4> Nome da tarefa: </h4> </label> <input type="text" value="{{$tarefa->nome ?? old('nome')}}" name="nome" minlength="4" maxlength="50" required> <br> <br>
-        <label> <h4> Descrição: </h4> </label>  <textarea name="descricao" placeholder="{{$tarefa->descricao ?? old('descricao')}}"  rows="10" cols="20" maxlength="100"> </textarea> <br> <br>
+                    @csrf
+                    @method('PUT')
+                    <label class="lead"> Nome da tarefa: </label> <br> <input type="text" value="{{$tarefa->nome ?? old('nome')}}" name="nome" minlength="4" maxlength="50" required> <br> <br>
+                    <label class="lead"> Descrição: </label> <br> <textarea name="descricao" placeholder="{{$tarefa->descricao ?? old('descricao')}}" rows="4" cols="50" maxlength="100"> </textarea> <br> <br>
 
-         <h4> Status: </h4> <select name="status_id" required>
-            <option>-- Selecione um Status --</option>
-            @foreach($status as $tarefa_status)
-             <option value="{{ $tarefa_status->id }}" {{ ($tarefa->status_id ?? old('status_id')) == $tarefa_status->id ? 'selected' : '' }}>{{ $tarefa_status->status }}</option>
-            @endforeach
-        </select> <br> <br> <br>
-        <button class="botao-editar" type="submit"> Editar </button>
-
-
-    </form>
+                    <label class="lead"> Status: </label> <br>
+                    <select name="status_id" required>
+                        <option>-- Selecione um Status --</option>
+                        @foreach($status as $tarefa_status)
+                        <option value="{{ $tarefa_status->id }}" {{ ($tarefa->status_id ?? old('status_id')) == $tarefa_status->id ? 'selected' : '' }}>{{ $tarefa_status->status }}</option>
+                        @endforeach
+                    </select> <br> <br> <br>
+                    <button type="submit" class="btn btn-primary">Editar Tarefa</button>
 
 
+                </form>
+
+                </form>
 
 
 
